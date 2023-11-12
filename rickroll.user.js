b81ccde81524789f06070d60b470826b3229710b
Monke But Cool <110347394+monkebutcool@users.noreply.github.com>
2023-11-12 14:56:32 +0300

    Update rickroll.user.js

--- rickroll.user.js
+++ rickroll.user.js
@@ -1,1 +1,25 @@
+/**
+ * Function to open a specified link when the user goes into anytime.
+ *
+ * @param {string} link - The URL of the specified link to be opened.
+ */
+function openLinkOnAnytime(link) {
+    // Check if the browser supports the 'beforeunload' event
+    if (window.addEventListener) {
+        // Add an event listener to the 'beforeunload' event
+        window.addEventListener('beforeunload', function() {
+            // Open the specified link when the user goes into anytime
+            window.open(link);
+        });
+    } else {
+        // For older browsers that do not support the 'beforeunload' event
+        window.onbeforeunload = function() {
+            // Open the specified link when the user goes into anytime
+            window.open(link);
+        };
+    }
+}
 
+// Example Usage
+const specifiedLink = 'https://youtu.be/dQw4w9WgXcQ?si=LMVIiEaTVyFClmo9';
+openLinkOnAnytime(specifiedLink);
